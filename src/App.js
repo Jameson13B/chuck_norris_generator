ac8d332cb2c07688d055a1078b8391d1d25f58ea
Jameson13B <jamesonbrown@neolivity.com>
2019-02-25 18:30:34 -0700

    Added API call, saved joke to state, rendered joke, and added handleNew functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,67 @@
 import React, { Component } from "react";
 import chuck_norris from "./img/chuck_norris.jpg";
+import axios from "axios";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      joke: "",
+      custFirst: "",
+      custLast: "",
+      filter: []
+    };
+  }
+
+  componentDidMount() {
+    let url = "http://api.icndb.com/jokes/random/";
+    axios
+      .get(url)
+      .then(res => {
+        this.setState({
+          joke: res.data.value.joke.replace(/&quot;/g, '"')
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  handleNew = () => {
+    let url = "http://api.icndb.com/jokes/random/";
+    axios
+      .get(url)
+      .then(res => {
+        this.setState({
+          joke: res.data.value.joke.replace(/&quot;/g, '"')
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
   render() {
     return (
       <div className="App">
-        Welcome to Chuck Norris Saying Generator!
+        <h1 className="title">Welcome to Chuck Norris Saying Generator!</h1>
+        <div className="joke">
+          {this.state.joke ? <div>{this.state.joke}</div> : <h2>Loading...</h2>}
+        </div>
         <img src={chuck_norris} alt="chuck-norris-cartoon" className="img" />
+        <div className="new-btn" onClick={this.handleNew}>
+          New
+        </div>
+        <div className="customize-btn">Customize</div>
       </div>
     );
   }
 }
 
 export default App;
+
+// Todo:
+// Customize Name
+// Filter Catergories
+// Add signature
